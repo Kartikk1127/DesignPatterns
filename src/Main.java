b61931bb561fefc19c1db655e935d3aeb50f2507
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2024-05-23 06:34:15 +0530

    factory method comment added in main class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+/*In this example, CarFactory is the factory class with the createCar() method, which
+is responsible for creating Car objects. ScorpioClassicFactory and ScorpioNFactory are
+subclasses of CarFactory that override the createCar() method to return
+ScorpioClassic and ScorpioN objects respectively*/
 public class Main {
     public static void main(String[] args) {
         CarFactory classicFactory = new ScorpioClassFactory();
