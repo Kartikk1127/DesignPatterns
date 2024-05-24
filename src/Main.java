3c52e66f574f2b7d7c6ef3bde0b482bb9d3d1a70
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2024-05-24 17:54:28 +0530

    builder pattern

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 //Create cars with different tyres, body and engine using Director and
 //Builder
+
+
+/*In this example, Car is the product class representing a car. CarBuilder is the
+abstract builder interface with methods for building different parts of the car.
+ScorpioBuilder and FortunerBuilder are concrete builders that implement the
+CarBuilder interface to build Scorpio and Fortuner cars respectively.
+CarManufacturer is the director class that orchestrates the construction process.
+Finally, the client code demonstrates how to use the builder pattern to build
+Scorpio and Fortuner cars with different configurations.*/
 public class Main {
     public static void main(String[] args) {
         CarManufacturer carManufacturer = new CarManufacturer();
